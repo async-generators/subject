57edc37e81707c3abaf59c407955e40969ed8869
Meirion Hughes <mhughes@geologging.com>
2017-09-29 16:15:28 +0100

    feat: emit disposed and pull events on subject

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,82 @@
 import { EventEmitter } from 'events';
+import terminator from '@async-generators/terminator';
+
+const $terminated = Symbol.for("terminated");
 
 export interface Subject<T> extends AsyncIterable<T> {
+  readonly isDisposed: boolean;
+
   next(item: T): number;
   error(err: any): void;
   done(): void;
+
+  on(event: "disposed", cb: () => void);
+  on(event: "pull", cb: (remaining: number) => void);
 }
 
-export default function <T>(
-): Subject<T> {
+function _Subject<T>() {
   let _signal = new EventEmitter();
   let _buffer: T[] = [];
   let _done: boolean = false;
   let _error: any;
 
-  return {
-    next: function (item: T) {
-      _buffer.push(item);
-      _signal.emit("tick");
-      return _buffer.length;
-    },
-    error: function (err: any) {
-      _error = err;
-      _signal.emit("tick");
-    },
-    done: function () {
-      _done = true;
-      _signal.emit("tick");
-    },
-    [Symbol.asyncIterator]: async function* () {
-      while (true) {
-        if (_error) {
-          throw _error;
+  function dispose() {
+    _done = true;
+    _signal.emit("disposed");
+  }
+
+  async function* iterator() {
+    while (true) {
+      if (_error) {
+        dispose();
+        throw _error;
+      }
+      if (_buffer.length === 0) {
+        if (_done) {
+          dispose();
+          return;
+        };
+        await new Promise((r) => {
+          _signal.once("push", r);
+        });
+      } else {
+        if ((yield _buffer.shift()) === $terminated) {
+          dispose();
+          return;
         }
-        if (_buffer.length === 0) {
-          if (_done) return;
-          await new Promise((r) => {
-            _signal.once("tick", r);
-          });
-        } else {
-          yield _buffer.shift();
-        }
+        _signal.emit("pull", _buffer.length);
       }
     }
   }
+
+  return {
+    get isDisposed() { return _done; },
+    next: function (item: T) {
+      if (_done) return -1;
+      _buffer.push(item);
+      _signal.emit("push");
+      return _buffer.length;
+    },
+    error: function (err: any) {
+      if (_done) return;
+      _error = err;
+      _signal.emit("push");
+    },
+    done: function () {
+      if (_done) return;
+      _done = true;
+      _signal.emit("push");
+    },
+    [Symbol.asyncIterator]: function () {
+      return terminator(iterator())[Symbol.asyncIterator]()
+    },
+    on(event: "disposed" | "pull", cb: (...args) => void) {
+      _signal.on(event, cb);
+    }
+  }
+}
+
+export default function <T>(
+): Subject<T> {
+  return _Subject<T>();
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -6,11 +6,11 @@ describe("@async-generator/subject", () => {
   it("should buffer all items before iteration", async () => {
     let source = subject<number>();
 
-    let expected = function*(){
-      yield 1; yield 2; yield 3; yield 4;    
+    let expected = function* () {
+      yield 1; yield 2; yield 3; yield 4;
     }
 
-    for(let item of expected()){
+    for (let item of expected()) {
       source.next(item);
     }
     source.done();
@@ -21,18 +21,60 @@ describe("@async-generator/subject", () => {
   it("should wait until items are pushed", async () => {
     let source = subject<number>();
 
-    let expected = function*(){
-      yield 1; yield 2; yield 3; yield 4;    
+    let expected = function* () {
+      yield 1; yield 2; yield 3; yield 4;
     }
 
     let result = equal(source, expected())
 
-    for(let item of expected()){
-      source.next(item);
-    }
-    source.done();
+    await new Promise(async done => {
+      for (let item of expected()) {
+        source.next(item);
+        await new Promise(r => setImmediate(r));
+      }
+      source.done();
+      done();
+    });
 
     expect(await result).to.be.true;
+  })
+
+  it("should dispose if consumer stops iteration", async () => {
+    let source = subject<number>();
+
+    source.next(1);
+    source.next(2);
+    source.next(3);
+    source.done();
+
+    await new Promise(async r => {
+      source.on("disposed", r);
+
+      for await (let item of source) {
+        break;
+      }
+    });
+  })
+
+  it("should raise pull event when consumer iterates", async () => {
+    let source = subject<number>();
+
+    source.next(1);
+    source.next(2);
+    source.next(3);
+    source.done();
+
+    let countPromise = new Promise(async r => {
+      source.on("pull", r);
+    });
+
+    let consumer = new Promise(r => source.on("pull", r));
+
+    for await (let item of source) { }
+
+    let count = await countPromise;
+
+    expect(count).to.be.eq(2);
   })
 
   it("should rethrow error", async () => {
@@ -42,10 +84,10 @@ describe("@async-generator/subject", () => {
 
     source.error(Error("pickle rick!"));
 
-    try{
+    try {
       await consumer
-    }catch(err){    
-      result = err;  
+    } catch (err) {
+      result = err;
     }
 
     expect(result.message).to.be.eq("pickle rick!");
